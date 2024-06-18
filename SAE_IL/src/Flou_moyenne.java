b06888583c895698445d3fd570396235093f98ab
unknown <maxime.bechard7@etu.univ-lorraine.fr>
2024-06-18 14:39:18 +0200

    calculfoumoyenne

--- SAE_IL/src/Flou_moyenne.java
+++ SAE_IL/src/Flou_moyenne.java
@@ -1,3 +1,19 @@
-public class Flou_moyenne {
+import java.awt.*;
+
+public class Flou_moyenne implements Flou{
+    public Color calcule(Color[][] couleurs){
+        int sum = couleurs.length * couleurs[0].length;
+        int r = 0;
+        int g = 0;
+        int b = 0;
+        for(int i = 0; i < couleurs.length; i++){
+            for(int j = 0; j < couleurs[0].length; j++){
+                r += couleurs[i][j].getRed()/sum;
+                g += couleurs[i][j].getGreen()/sum;
+                b += couleurs[i][j].getBlue()/sum;
+            }
+        }
+        return new Color(r,g,b);
+    }
 
 }
