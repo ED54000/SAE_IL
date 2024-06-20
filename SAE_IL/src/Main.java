78f59b7b0a3eac0144cec3abdb585cf4fe16da47
ED54000 <eloi_duchene@orange.fr>
2024-06-20 15:36:52 +0200

    Modification du main

--- SAE_IL/src/Main.java
+++ SAE_IL/src/Main.java
@@ -7,47 +7,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) throws IOException {
         Image image = new Image();
-
-        String  path = "C:/Users/user/Desktop/Cours/SAE-Planet/SAE_IL/SAE_IL/img/";
-        //try {
-        // image.copy_image("src/", "img.jpg", "image","png");
-        //  } catch (IOException e) {
-        //     System.out.println(e);
-        //}
-        //image.copy_by_pixel("src/", "img.jpg", "image_pixel","png");
-        //image.copy_black_and_white("src/", "img.jpg", "image_bw","png");
-        // image.copy_red("src/", "img.jpg", "image_red","png");
-        //image.copy_blue_green("src/", "img.jpg", "image_blue_green","png");
-
-        //image.modif_color("src/", "img.jpg", "image_modif3","png");
-        //  image.modif_oeil("src/", "img.jpg", "image_modif4","png");
-        //image.flouter_moyenne("src/", "img.jpg", "image_flou","png");
-
-        //image.flouter_gaussienne("img/", "Planete 2.jpg", "Planete_Floue","png");
-
-        /*
->>>>>>> 9008e5f3b815bd25dcc10fd54bf8834834cd0883
-        // algo K-Mean
-        K_Mean scan = new K_Mean(200);
-        int[][] param = image.image_to_param("C:/Users/flori/Documents/Travail IUT/S4/SAE/SAE - Détection de biomes sur des exoplanètes/SAE_IL/SAE_IL/img/", "test2.jpg");
-        int[] clusters = scan.algoClust(param);
-        System.out.println(Arrays.toString(clusters));
-        String[] biomes = image.convertCluster(param, clusters);
-
-        // liste des biomes présents
-        List<String> biomePresents = new ArrayList<>();
-        for (String biome : biomes) {
-            if (!biomePresents.contains(biome)) {
-                biomePresents.add(biome);
-            }
-        }
-        System.out.println("Liste des biomes présents : " + biomePresents);
-
-        int[] dim = image.getDim("C:/Users/flori/Documents/Travail IUT/S4/SAE/SAE - Détection de biomes sur des exoplanètes/SAE_IL/SAE_IL/img/", "test2.jpg");
-        image.imagebiome(param, biomes, clusters, "C:/Users/flori/Documents/Travail IUT/S4/SAE/SAE - Détection de biomes sur des exoplanètes/SAE_IL/SAE_IL/img/", "test2",  "png", dim[0], dim[1]);
-        //image.afficherBiomes("img/", "Planete 2.jpg", "Fond","png");
-        */
-/*
+        String  path =  "C:/Users/grand/OneDrive/Documents/cours/2eme_annee/S4/SAE/SAE_IL/SAE_IL/img/";
         String[] images = {"Planete 1", "Planete 2", "Planete 3", "Planete 4", "Planete 5"};
         String[] normes = {"Redmean", "CIELAB", "Oeil"};
         int flou = 5;
@@ -67,22 +27,15 @@ public class Main {
                 System.out.println("Image " + img + " avec la norme " + normes[i] + " a été traitée");
             }
         }
-        */
+
         int[] dim = image.getDim(path, "Planete 1.jpg");
         DBScan scan = new DBScan(40,12, new NormeCIELAB(),dim[0]);
         int[][] param = image.image_to_param(path, "Planete_Floue.png");
         int[] clusters = scan.algoClust(param);
         String[] biomes = image.convertCluster(param, clusters);
-        //image.imagebiome(param, biomes, clusters, path+"/dbscan", img+ "DBSCAN "+ normes[i], ".png", dim[0], dim[1]);
-        image.biomeFondBlanc(param, biomes, clusters, path, "Planete 1.jpg","Planete 1 FondBiomes", "png", dim[0], dim[1]);
 
-        /*DBScan scan = new DBScan(3, 5);
-        int[][] param = image.image_to_param("C:/Users/user/Desktop/Cours/SAE-Planet/SAE_IL/SAE_IL/img/", "Planete_Floue.png");
-        int[] clusters = scan.algoClust(param);
-        System.out.println(Arrays.toString(clusters));
-        String[] biomes = image.convertCluster(param, clusters);
-        int[] dim = image.getDim("C:/Users/user/Desktop/Cours/SAE-Planet/SAE_IL/SAE_IL/img/", "test2.jpg");
-        image.imagebiome(param, biomes, clusters, "C:/Users/user/Desktop/Cours/SAE-Planet/SAE_IL/SAE_IL/img/", "test2",  "png", dim[0], dim[1]);
-        //image.afficherBiomes("img/", "Planete 2.jpg", "Fond","png");*/
+        image.biomeFondBlanc(param, biomes, clusters, path, "Planete 1.jpg","fonds/Planete 1 FondBiomes", "png", dim[0], dim[1]);
+
+
     }
 }
