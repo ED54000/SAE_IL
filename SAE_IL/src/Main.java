af9ab35d0edbcf83d6be46044def0e1038d4a44d
Fiori4444 <florian.bortolotti88@gmail.com>
2024-06-20 09:09:30 +0200

    petite modif main

--- SAE_IL/src/Main.java
+++ SAE_IL/src/Main.java
@@ -1,6 +1,8 @@
 import java.awt.*;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -19,13 +21,23 @@ public class Main {
         //  image.modif_oeil("src/", "img.jpg", "image_modif4","png");
         //image.flouter_moyenne("src/", "img.jpg", "image_flou","png");
         //image.flouter_gaussienne("img/", "Planete 2.jpg", "Planete_Floue","png");
-        DBScan scan = new DBScan(3, 5);
-        int[][] param = image.image_to_param("C:/Users/user/Desktop/Cours/SAE-Planet/SAE_IL/SAE_IL/img/", "test2.jpg");
+        K_Mean scan = new K_Mean(200);
+        int[][] param = image.image_to_param("C:/Users/flori/Documents/Travail IUT/S4/SAE/SAE - Détection de biomes sur des exoplanètes/SAE_IL/SAE_IL/img/", "test2.jpg");
         int[] clusters = scan.algoClust(param);
         System.out.println(Arrays.toString(clusters));
         String[] biomes = image.convertCluster(param, clusters);
-        int[] dim = image.getDim("C:/Users/user/Desktop/Cours/SAE-Planet/SAE_IL/SAE_IL/img/", "test2.jpg");
-        image.imagebiome(param, biomes, clusters, "C:/Users/user/Desktop/Cours/SAE-Planet/SAE_IL/SAE_IL/img/", "test2",  "png", dim[0], dim[1]);
+
+        // liste des biomes présents
+        List<String> biomePresents = new ArrayList<>();
+        for (String biome : biomes) {
+            if (!biomePresents.contains(biome)) {
+                biomePresents.add(biome);
+            }
+        }
+        System.out.println("Liste des biomes présents : " + biomePresents);
+
+        int[] dim = image.getDim("C:/Users/flori/Documents/Travail IUT/S4/SAE/SAE - Détection de biomes sur des exoplanètes/SAE_IL/SAE_IL/img/", "test2.jpg");
+        image.imagebiome(param, biomes, clusters, "C:/Users/flori/Documents/Travail IUT/S4/SAE/SAE - Détection de biomes sur des exoplanètes/SAE_IL/SAE_IL/img/", "test2",  "png", dim[0], dim[1]);
         //image.afficherBiomes("img/", "Planete 2.jpg", "Fond","png");
     }
 }
